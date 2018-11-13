a7216cbfae32af2649b04fda75a139f5be70aef1
Rexman17 <a.stone1794@gmail.com>
2018-11-13 12:11:39 -0500

    wtf

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -42,8 +42,4 @@ class DishesController < ApplicationController
   def find_dish
     @dish = Dish.find_by(id: params[:id])
   end
-<<<<<<< HEAD
-=======
-
->>>>>>> dish controller and associate views
 end # end of controller
