905c2242bc7f7318b3ccfad04d27de00d1fb9dc9
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-11-13 13:31:09 -0500

    that dank commit  - time selector

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,7 @@ class Event < ApplicationRecord
   # Validation
   validates_presence_of :name, on: :create, message: "Please pick a name for your potluck"
   validates_presence_of :date, on: :create, message: "Please pick a date for your potluck"
+  validates_presence_of :time, on: :create, message: "Please pick a time for your potluck"
   validates_presence_of :location, on: :create, message: "Please pick a location for your potluck"
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_182904) do
+ActiveRecord::Schema.define(version: 2018_11_13_182153) do
   create_table "dishes", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
@@ -29,10 +29,11 @@ ActiveRecord::Schema.define(version: 2018_11_13_182904) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "description"
     t.string "location"
+    t.datetime "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
