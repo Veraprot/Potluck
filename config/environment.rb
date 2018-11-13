b5e3a07bf199f7e6e9b3335afb4d15985c6320bd
Veraprot <vera.protopopova@gmail.com>
2018-11-13 13:09:03 -0500

    Remove files and edits from last merge

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Paperclip.options[:command_path] = "/usr/local/bin/"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root to: 'home#index'
   resources :events, only: %i[index new show create edit update]
+  resources :users , only: %i[index show new create]
+
 end
