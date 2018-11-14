78ac7aa0d4b3045c80bcec18dd696306623d6f63
Veraprot <vera.protopopova@gmail.com>
2018-11-14 12:00:01 -0500

    add user experience

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
 
   before_action :find_event, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only:[:create,:edit,:destroy]
+  before_action :authenticate_user!, only:[:new, :create,:edit,:destroy]
   
 
   def index
